8a0ead79a85c1cd883bc18937421137280926db7
Daniel Hey <danielhey@outlook.com>
2023-05-02 13:04:32 -0400

    badge

--- get_citations.py
+++ get_citations.py
@@ -2,7 +2,10 @@ import ads
 import matplotlib.dates as mdates
 import pandas as pd
 import matplotlib.pyplot as plt
+import json
+
 from ads_fields import FIELDS
+
 
 def query_ads() -> pd.DataFrame:
     """Queries NASA ADS for papers referencing lightkurve
@@ -10,8 +13,10 @@ def query_ads() -> pd.DataFrame:
     Returns:
         pd.DataFrame: Output statistics
     """
-    qry = ads.SearchQuery(q='full:"lightkurve" AND year:2017-2050', rows=999999, fl=FIELDS)
-    papers = [q for q in qry ]
+    qry = ads.SearchQuery(
+        q='full:"lightkurve" AND year:2017-2050', rows=999999, fl=FIELDS
+    )
+    papers = [q for q in qry]
 
     dates = [p.date for p in papers[::-1]]
     titles = [p.title[0] for p in papers[::-1]]
@@ -21,19 +26,24 @@ def query_ads() -> pd.DataFrame:
     pubs = [p.pub for p in papers[::-1]]
     cite_count = [p.citation_count for p in papers[::-1]]
 
-    df = pd.DataFrame({'year': years,
-                    'date': pd.to_datetime(dates),
-                    'title': titles,
-                    'author': authors,
-                    'bibcode': bibcodes,
-                    'pub': pubs,
-                    'cite_count': cite_count})
+    df = pd.DataFrame(
+        {
+            "year": years,
+            "date": pd.to_datetime(dates),
+            "title": titles,
+            "author": authors,
+            "bibcode": bibcodes,
+            "pub": pubs,
+            "cite_count": cite_count,
+        }
+    )
     # Filter out Zenodo entries and AAS Abstracts
     mask = ~df.pub.str.contains("(Zenodo)|(Abstracts)")
     # Sort by date and reset index
-    df = df[mask].sort_values('date', ascending=False).reset_index(drop=True)
+    df = df[mask].sort_values("date", ascending=False).reset_index(drop=True)
 
     return df
+
 
 def make_recent_table(df) -> pd.DataFrame:
     """Formats and makes a nice table of the 5 most recent papers which cite lightkurve
@@ -46,36 +56,41 @@ def make_recent_table(df) -> pd.DataFrame:
     """
 
     ## Make a markdown table
-    most_recent = df.sort_values('date', ascending=False).head(5)
-    most_recent = most_recent.rename(columns={'date': 'Date', 'title': 'Title', 'author': 'Author'})
+    most_recent = df.sort_values("date", ascending=False).head(5)
+    most_recent = most_recent.rename(
+        columns={"date": "Date", "title": "Title", "author": "Author"}
+    )
     link_title = []
     for index, row in most_recent.iterrows():
-        link_title.append(f'[{row.Title}](https://ui.adsabs.harvard.edu/abs/{row.bibcode}/abstract)')
-    most_recent['Title'] = link_title
-    most_recent['Date'] = most_recent['Date'].dt.date
-    recent_table = most_recent[['Date', 'Title', 'Author']]
+        link_title.append(
+            f"[{row.Title}](https://ui.adsabs.harvard.edu/abs/{row.bibcode}/abstract)"
+        )
+    most_recent["Title"] = link_title
+    most_recent["Date"] = most_recent["Date"].dt.date
+    recent_table = most_recent[["Date", "Title", "Author"]]
     return recent_table
 
-def make_readme(md, path='README.md'):
+
+def make_readme(md, path="README.md"):
     """Makes a readme file containing the publications plot and recent papers
 
     Args:
         md (_type_): markdown to include
         path (str, optional): output path. Defaults to 'README.md'.
     """
-    readme_str = f'''
+    readme_str = f"""
 <h1>Lightkurve statistics</h1>
 
 ![publications](out/lightkurve-publications.png)  
 
 {md}
-    '''
+    """
     text_file = open(path, "w")
     n = text_file.write(readme_str)
     text_file.close()
 
-def make_plot(df : pd.DataFrame, 
-path='out/lightkurve-publications.png'):
+
+def make_plot(df: pd.DataFrame, path="out/lightkurve-publications.png"):
     """Generates and saves a lightkurve publications plot
 
     Args:
@@ -83,13 +98,13 @@ path='out/lightkurve-publications.png'):
         path (str, optional): output path. Defaults to 'out/lightkurve-publications.png'.
     """
     # Make a plot
-    x = pd.date_range('2018-01-01T00:00:00Z', df.date.max(), freq='1M')
+    x = pd.date_range("2018-01-01T00:00:00Z", df.date.max(), freq="1M")
     y = [len(df[df.date < d]) for d in x]
 
-    plot_color = '#777'
+    plot_color = "#777"
     fig, ax = plt.subplots(figsize=[9, 5])
-    ax.plot(x, y, marker='o', c=plot_color)
-    ax.set_xlabel('Year', fontsize=15, c=plot_color)
+    ax.plot(x, y, marker="o", c=plot_color)
+    ax.set_xlabel("Year", fontsize=15, c=plot_color)
     ax.set_ylabel("Publications", fontsize=15, c=plot_color)
     locator = mdates.AutoDateLocator(minticks=3, maxticks=7)
     formatter = mdates.ConciseDateFormatter(locator)
@@ -102,12 +117,21 @@ path='out/lightkurve-publications.png'):
     plt.savefig(path, transparent=True)
     plt.close()
 
-if __name__ == '__main__':
+
+def make_badge_endpoint(df):
+    badge = {"color": "blue", "status": f"{len(df)}", "subject": "Citations"}
+    json_object = json.dumps(badge, indent=4)
+    # Writing to sample.json
+    with open("out/badge.json", "w") as outfile:
+        outfile.write(json_object)
+
+
+if __name__ == "__main__":
     # Query ADS
     df = query_ads()
 
     # Save entire statistics table
-    df.to_csv('out/statistics.csv')
+    df.to_csv("out/statistics.csv")
 
     # get recent publications
     recent = make_recent_table(df)
@@ -117,3 +141,5 @@ if __name__ == '__main__':
 
     # Now save the readme
     make_readme(recent.to_markdown())
+
+    make_badge_endpoint(df)
